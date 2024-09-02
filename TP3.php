b6ac00af7687d90f8f77d96ebddf29fc41d39f26
David Liva <livadavid78@gmail.com>
2024-09-02 14:39:18 -0300

    Se han evaluado los valores ingresados por los input para determinar el estado academico de cada alumno

--- TP3.php
+++ TP3.php
@@ -7,21 +7,39 @@ for ($student=0; $student < 10 ; $student++) {
 
     for ($cantNotas=0; $cantNotas < 2 ; $cantNotas++) { 
         $nota = readline("Ingrese la calificación del parcial " . ($cantNotas + 1) . ": ");
-
         
         array_push($notas, $nota);
-        // array_push($nota, readline("Ingrese la calificación del primer parcial: "));
-        // array_push($nota, readline("Ingrese la calificación del segundo parcial: "));
     }
+
+    $DNI = readline("Ingrese el DNI del alumno:");
+
+    $nota1 = $notas[0];
+    $nota2 = $notas[1];
+    print_r("$nota1 \n");
+    print_r("$nota2 \n");
+    
+    
     for ($cantAsistencia=0; $cantAsistencia < 1; $cantAsistencia++) { 
         $asistencia = readline("Ingrese la asistencia en números entero (max: 40): ");
         $calAsistencia = ($asistencia / 40) * 100;
     }
+
+    if((($nota1 >= 8) && ($nota2 >= 8)) && ($calAsistencia >= 80)){
+        print_r("Alumno regular: $DNI \n");
+    }elseif ((($nota1 > 8) && ($nota2 > 8)) && ($calAsistencia < 80)) {
+        print_r("Debe realizar clases de apoyo: $DNI \n");
+    }elseif ((($nota1 < 8) || ($nota2 < 8)) && ($calAsistencia >= 80)) {
+        print_r("Debe recuperar un parcial: $DNI \n");
+    }else{
+        print_r("Alumno libre: $DNI \n");
+    }
+    
     $estudiante = array(
         "nombre" => $nombre,
         "notas" => $notas,
         "asistencia %" => $calAsistencia
     );
+    
 
     array_push($estudiantes, $estudiante);
     print_r($estudiantes);
